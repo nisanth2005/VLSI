e095aa4de884dc99833d7a0178d10afddd606341
Nisanth <nisanth12.2005@gmail.com>
2026-05-24 13:17:51 +0530

    Update Testbench.v

--- Projects/Smart Cold Storage Temperature Controller/Testbench.v
+++ Projects/Smart Cold Storage Temperature Controller/Testbench.v
@@ -1,1 +1,32 @@
-
+`timescale 1ns / 1ps
+module testbench;
+reg clk;
+reg reset;
+reg [7:0] temp;
+wire [1:0] cooling_level;
+wire alert;
+topmodule uut(
+    .clk(clk),
+    .reset(reset),
+    .temp(temp),
+    .cooling_level(cooling_level),
+    .alert(alert)
+);
+always #5 clk = ~clk;
+initial
+begin
+clk = 0;
+reset = 1;
+temp = 20;
+#10 reset = 0;
+#20 temp = 22;
+#20 temp = 26;
+#20 temp = 30;
+#20 temp = 33;
+#20 temp = 36;
+#20 reset = 1;
+#10 reset = 0;
+#20 temp = 21;
+#50 $finish;
+end
+endmodule
