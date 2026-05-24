b3f7fa79b36782bf8db91a50cd9228b2f08dff6e
Nisanth <nisanth12.2005@gmail.com>
2026-05-24 11:28:54 +0530

    Update Testbench.v

--- Projects/PWM Based DC Motor Speed and Direction Control using Verilog HDL/Testbench.v
+++ Projects/PWM Based DC Motor Speed and Direction Control using Verilog HDL/Testbench.v
@@ -1,1 +1,45 @@
 
+`timescale 1ns/1ps
+module pwm_tb;
+    reg clk;
+    reg rst;
+    reg [1:0] value;
+    reg dir;
+    wire in1, in2;
+    pwm uut (
+        .clk(clk),
+        .rst(rst),
+        .value(value),
+        .dir(dir),
+        .in1(in1),
+        .in2(in2)
+    );
+    initial begin
+        clk = 0;
+        forever #5 
+        clk = ~clk;
+    end
+    initial begin
+
+        rst   = 1;
+        value = 2'b00;
+        dir   = 0;
+        #10;
+        rst = 0;
+        #10;
+        value = 2'b01;
+        #10;
+        dir = 1;
+        #10;
+        value = 2'b10; 
+         dir = 0;
+        #10;
+        dir = 1;
+        #10;
+        value = 2'b11; dir = 0;
+        #10;
+        dir = 1;
+        #10;
+        $stop;
+    end
+endmodule
